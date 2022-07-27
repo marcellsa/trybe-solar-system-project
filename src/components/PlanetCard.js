369096fc8b75750a1e5c9297af8f2a990364c162
Marcel Albuquerque <msalbuquerque@hotmail.com>
2022-07-26 23:28:26 -0300

    Requisito 5 implementado.

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    const altText = `Planeta ${planetName}`;
+
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{planetName}</p>
+        <img src={ planetImage } alt={ altText } />
+
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.node.isRequired,
+};
+
+export default PlanetCard;
